53807ed862e480ad5b7d81b36e97e47765989778
Melanie Kat <318003+ZicklePop@users.noreply.github.com>
2020-10-27 19:43:02 -0700

    nit

--- next.config.js
+++ next.config.js
@@ -2,16 +2,9 @@ const withCss = require('@zeit/next-css')
 const withPurgeCss = require('next-purgecss')
 const withPlugins = require('next-compose-plugins')
 
-const nextConfig = {
-  images: {
-    iconSizes: [256]
-  }
-}
-
 module.exports = withPlugins(
   [
     [withCss],
     [withPurgeCss]
-  ],
-  nextConfig
+  ]
 )
