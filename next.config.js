2f9cc39fe3d30bbf0bce2ad6e57bb768eb6d05ed
Melanie Kat <318003+ZicklePop@users.noreply.github.com>
2021-10-27 19:39:57 +0000

    next12 config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,14 @@
-const withPlugins = require('next-compose-plugins')
+const withPlugins = require("next-compose-plugins");
 
 const nextConfig = {
+  swcMinify: true,
+  images: {
+    formats: ["image/avif", "image/webp"],
+  },
   i18n: {
-    locales: ['en', 'pl', 'ja'],
-    defaultLocale: 'en'
-  }
-}
+    locales: ["en", "pl", "ja"],
+    defaultLocale: "en",
+  },
+};
 
-module.exports = withPlugins(
-  [],
-  nextConfig
-)
+module.exports = withPlugins([], nextConfig);
