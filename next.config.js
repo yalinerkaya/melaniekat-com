095eb0f1d85a4987c3cb3c19802fc448839dee7b
Melanie Kat <318003+ZicklePop@users.noreply.github.com>
2022-08-31 05:07:29 +0000

    changing image

--- next.config.js
+++ next.config.js
@@ -1,14 +1,9 @@
-const withPlugins = require("next-compose-plugins");
-
 const nextConfig = {
   swcMinify: true,
-  images: {
-    formats: ["image/avif", "image/webp"],
-  },
   i18n: {
     locales: ["en", "pl", "ja"],
     defaultLocale: "en",
   },
 };
 
-module.exports = withPlugins([], nextConfig);
+module.exports = nextConfig;
--- pages/index.js
+++ pages/index.js
@@ -1,50 +1,38 @@
-import Image from 'next/image'
-import Layout from '../components/layout'
-import Link from 'next/link'
-import ListLink from '../components/list-link'
-import comission from '../public/uglytarantula-commission.jpg'
-import useI18n from '../hooks/use-i18n'
-import { useRouter } from 'next/router'
+import Layout from "../components/layout";
+import Link from "next/link";
+import ListLink from "../components/list-link";
+import Logo from "../components/logo";
+import useI18n from "../hooks/use-i18n";
+import { useRouter } from "next/router";
 
 const colors = {
-  gween: '#65A300',
-  mel: '#0096DB',
-  pweach: '#FF5757',
-  pwink: '#FF52A3'
-}
+  gween: "#65A300",
+  mel: "#0096DB",
+  pweach: "#FF5757",
+  pwink: "#FF52A3",
+};
 
 const cx = {
-  main: 'vh-100 dt w-100',
-  container: 'dtc v-mid tc',
-  article: 'measure-narrow center sans-serif f3 fw2 ph2',
-  h1: 'lh-title fw2 f2',
-  h1a: 'no-underline',
-  ul: 'list pl0 tl',
-  liTop: 'lh-title mb2 mt4',
-  img: 'br-100 center dib ma3 overflow-hidden'
-}
+  main: "vh-100 dt w-100",
+  container: "dtc v-mid tc",
+  article: "measure-narrow center sans-serif f3 fw2 ph2",
+  h1: "lh-title fw2 f2",
+  h1a: "no-underline",
+  ul: "list pl0 tl",
+  liTop: "lh-title mb2 mt4",
+  img: "center dib ma3",
+};
 
 const Index = () => {
-  const t = useI18n(useRouter())
+  const t = useI18n(useRouter());
   return (
     <Layout className={cx.main}>
       <div className={cx.container}>
         <article className={cx.article}>
-          <div className={cx.img} style={{ height: 256, width: 256 }}>
-            <Image
-              alt={t('image-description')}
-              height={256}
-              loading='eager'
-              src={comission}
-              placeholder='blur'
-              width={256}
-            />
-          </div>
+          <Logo alt={t("image-description")} className={cx.img} width={256} />
           <h1 className={cx.h1}>
-            <Link href='/'>
-              <a className={cx.h1a}>
-                {t('introduction')}
-              </a>
+            <Link href="/">
+              <a className={cx.h1a}>{t("introduction")}</a>
             </Link>
           </h1>
 
@@ -52,67 +40,67 @@ const Index = () => {
             <ListLink
               className={cx.liTop}
               color={colors.pwink}
-              prefix={t('gf-prefix')}
-              text={t('gf-link-label')}
-              postfix={t('gf-postfix')}
-              rel='me'
-              url='https://girlfriend.technology'
+              prefix={t("gf-prefix")}
+              text={t("gf-link-label")}
+              postfix={t("gf-postfix")}
+              rel="me"
+              url="https://girlfriend.technology"
             />
             <ListLink
               className={cx.liTop}
-              prefix={t('blog-prefix')}
-              text={t('blog-link-label')}
-              postfix={t('blog-postfix')}
-              rel='me'
-              url='https://melkat.link'
+              prefix={t("blog-prefix")}
+              text={t("blog-link-label")}
+              postfix={t("blog-postfix")}
+              rel="me"
+              url="https://melkat.link"
             />
             <ListLink
-              prefix={t('gifs-prefix')}
-              text={t('gifs-link-label')}
-              postfix={t('gifs-postfix')}
-              rel='me'
-              url='https://myhot.pics'
+              prefix={t("gifs-prefix")}
+              text={t("gifs-link-label")}
+              postfix={t("gifs-postfix")}
+              rel="me"
+              url="https://myhot.pics"
             />
             <ListLink
-              prefix={t('deals-prefix')}
-              text={t('deals-link-label')}
-              postfix={t('deals-postfix')}
-              rel='me'
-              url='https://melkat.deals'
+              prefix={t("deals-prefix")}
+              text={t("deals-link-label")}
+              postfix={t("deals-postfix")}
+              rel="me"
+              url="https://melkat.deals"
             />
 
             <ListLink
               className={cx.liTop}
-              prefix={t('github-prefix')}
-              text={t('github-link-label')}
-              postfix={t('github-postfix')}
-              rel='me'
-              url='https://github.com/zicklepop'
+              prefix={t("github-prefix")}
+              text={t("github-link-label")}
+              postfix={t("github-postfix")}
+              rel="me"
+              url="https://github.com/zicklepop"
             />
 
             <ListLink
-              prefix={t('art-by-prefix')}
-              text={t('art-by-link-label')}
-              postfix={t('art-by-postfix')}
+              prefix={t("art-by-prefix")}
+              text={t("art-by-link-label")}
+              postfix={t("art-by-postfix")}
               className={cx.liTop}
               color={colors.pweach}
-              url='https://rosygaze.com'
+              url="https://dzuk.zone"
             />
           </ul>
         </article>
       </div>
       <style global jsx>
         {`
-            ul > li::before {
-              content: '*';
-              color: #8F8F8F;
-              width: 1rem;
-              display: inline-block;
-            }
+          ul > li::before {
+            content: "*";
+            color: #8f8f8f;
+            width: 1rem;
+            display: inline-block;
+          }
         `}
       </style>
     </Layout>
-  )
-}
+  );
+};
 
-export default Index
+export default Index;
