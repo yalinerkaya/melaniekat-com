cb362149ad40caf64dfe415d6287cfbbc5020857
Melanie Kat <318003+ZicklePop@users.noreply.github.com>
2022-12-12 00:50:16 +0000

    trying a content header

--- pages/api/webfinger.js
+++ pages/api/webfinger.js
@@ -4,5 +4,6 @@ const MY_FINGER =
 export default async function finger(_, res) {
   const data = await fetch(MY_FINGER);
   const json = await data.json();
+  res.setHeader("content-type", "application/activity+json");
   res.status(200).json(json);
 }
